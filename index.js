69d11ea3b7b5ee739323c27e8204baa734020bc9
Rohit Patil <rohitp@bsf.io>
2024-10-23 21:34:25 +0530

    fix: AI changes

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ class PullRequestReviewer {
                                                                     "position":
                                                                         {
                                                                             "type": "number",
-                                                                            "description": "The position in the diff where you want to add a review comment. The position value equals the number of lines down from the first \"@@\" hunk header in the file you want to add a comment. The line just below the \"@@\" line is position 1, the next line is position 2, and so on. The position in the diff continues to increase through lines of whitespace and additional hunks until the beginning of a new file."
+                                                                            "description": "The position in the diff where you want to add a review comment. Sololy to + line. The position value equals the number of lines down from the first \"@@\" hunk header in the file you want to add a comment. The line just below the \"@@\" line is position 1, the next line is position 2, and so on. The position in the diff continues to increase through lines of whitespace and additional hunks until the beginning of a new file."
                                                                         },
                                                                     "side":
                                                                         {
